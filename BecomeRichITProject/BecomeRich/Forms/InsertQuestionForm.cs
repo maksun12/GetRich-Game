25b922bf9808d0e15cfdb31da0134e25f962fa01
Ani <ani_hadjiasanova@abv.bg>
2019-03-23 14:01:09 +0200

    Update InsertQuestionForm(Add code)

--- BecomeRichITProject/BecomeRich/Forms/InsertQuestionForm.cs
+++ BecomeRichITProject/BecomeRich/Forms/InsertQuestionForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BecomeRich.Controllers;
+using BecomeRich.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,93 @@ namespace BecomeRich.Forms
         public InsertQuestionForm()
         {
             InitializeComponent();
+            comboBox1.Items.Add("Лесно");
+            comboBox1.Items.Add("Средно");
+            comboBox1.Items.Add("Трудно");
+            comboBox1.Text= comboBox1.Items[0].ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            InsertController insert = new InsertController();
+            MessegeApp message = new MessegeApp();
+            
+            string qustion = textBox1.Text;
+            string answearA ="A: " + textBox2.Text;
+            string answearB ="B: " + textBox3.Text;
+            string answearC ="C: " + textBox4.Text;
+            string answearD ="D: " + textBox5.Text;
+           string correctAnswear=string.Empty;
+            if (radioButton1.Checked)
+            {
+                correctAnswear = answearA;
+            }
+            else if (radioButton2.Checked)
+            {
+                correctAnswear = answearB;
+            }
+            else if (radioButton3.Checked)
+            {
+                correctAnswear = answearC;
+            }
+           else  if (radioButton4.Checked)
+            {
+                correctAnswear = answearD;
+            }
+
+            int category=0;
+            if (comboBox1.Text=="Трудно")
+            {
+                category = 3;
+            }
+            else if (comboBox1.Text == "Средно")
+            {
+                category = 2;
+            }
+            else
+            { 
+                category = 1;
+            }
+
+
+
+            bool isAdd = false;
+            if (qustion!=string.Empty && answearA != string.Empty && answearB != string.Empty && answearC != string.Empty && answearD != string.Empty )
+
+            {
+                isAdd = insert.InsertIntoQuestions(qustion, answearA, answearB, answearC, answearD, correctAnswear, category);
+                if (isAdd)
+                {
+                    MessageBox.Show(message.MessegeInsertQuestionsTrue(qustion, answearA, answearB, answearC, answearD, correctAnswear, category));
+
+
+                }
+                else
+                {
+                    MessageBox.Show(message.MessegeInsertQuestionsFalse(qustion, answearA, answearB, answearC, answearD, correctAnswear, category));
+                   
+                }
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                textBox4.Text = string.Empty;
+                textBox5.Text = string.Empty;
+              
+                comboBox1.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Въведи нужните данни в полетата!");
+            }
+
+        }
+
+        
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
