09f47c42a15befaad43d625cadcf0ad1fe989973
Danny Kichilieva <Dani_Kichilieva@abv.bg>
2019-03-23 10:18:57 +0200

    Add Code to InsertCountryForm.cs

--- BecomeRichITProject/BecomeRich/Forms/InsertCountryForm.cs
+++ BecomeRichITProject/BecomeRich/Forms/InsertCountryForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BecomeRich.Controllers;
+using BecomeRich.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,37 @@ namespace BecomeRich.Forms
 {
     public partial class InsertCountryForm : Form
     {
+        public object InsertControler { get; private set; }
         public InsertCountryForm()
         {
+
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InsertController insert = new InsertController();
+            MessegeApp message = new MessegeApp();
+            string countryName = textBox1.Text;
+            bool isAdd = false;
+            if (countryName != string.Empty)
+            {
+                isAdd = insert.InsertIntoCountries(countryName);
+                if (isAdd == true)
+                {
+                    MessageBox.Show(message.MessegeInsertCountryTrue(countryName));
+                }
+                else
+                {
+                    MessageBox.Show(message.MessegeInsertCountryFalse(countryName));
+                }
+                textBox1.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Въведи име на държава!");
+            }
+
         }
     }
 }
