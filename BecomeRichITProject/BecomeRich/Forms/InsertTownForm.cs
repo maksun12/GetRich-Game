3196225cec401d0982642d69e959a109dcd136a2
Ani <ani_hadjiasanova@abv.bg>
2019-04-02 10:31:47 +0300

    Add code to InsertTownForm

--- BecomeRichITProject/BecomeRich/Forms/InsertTownForm.cs
+++ BecomeRichITProject/BecomeRich/Forms/InsertTownForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BecomeRich.Controllers;
+using BecomeRich.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,39 @@ namespace BecomeRich.Forms
 {
     public partial class InsertTownForm : Form
     {
-        public InsertTownForm()
+        InsertController insert;
+        MessegeApp message;
+        public InsertTownForm(InsertController insert, MessegeApp message)
         {
+            this.insert = insert;
+            this.message = message;
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string townName = textBox1.Text;
+            string countryName = textBox2.Text;
+
+            bool isAdd = false;
+            if (townName != string.Empty && countryName != string.Empty)
+            {
+                isAdd = insert.InsertIntoTowns(townName, countryName);
+                if (isAdd == true)
+                {
+                    MessageBox.Show(message.MessegeInsertTownTrue(countryName, townName));
+                }
+                else
+                {
+                    MessageBox.Show(message.MessegeInsertTownFalse(countryName, townName));
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Въведи име на държава и град!");
+            }
         }
     }
 }
+
